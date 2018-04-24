9c18171cdc4c50d1d3ea902620b6622f2411cfb3
Behzad Khosravifar <behzad.khosravifar@gmail.com>
2018-04-24 13:46:16 +0430

    update libraries

--- src/BallSimulate2D/Balls - By VB.PowerPacks/Balls/Ball.cs
+++ src/BallSimulate2D/Balls - By VB.PowerPacks/Balls/Ball.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualBasic.PowerPacks;
 
 namespace Balls
 {
